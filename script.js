3d54f784c88380f84c5fa37aa9a0fb0b90dfb314
OscarOrtiz <tippingtheiceberg@gmail.com>
2022-09-30 23:24:05 -0500

    fix lag solution by moving color selection outside of for loop

--- script.js
+++ script.js
@@ -7,11 +7,13 @@
 
 // 4: Once complete, this will be the default grid we start with, now make it so that the user can change the amount of grids with the button click that should open a prompt (COMPLETE)
 //      a: newly created divs are added onto previous created ones, old ones need to be removed before new ones are added.
+//      b: grids greater than/around 30 cause delay, when changing, fix this. (IN PROGRESS) 
 
 // 5: When we hover over a div, the background color should change to black from the default white and stay black when we leave it.(COMPLETE)
 
 // 6: Make the colors random when we hover over them. 
-//      a: colors must retain their original color and do not change over each re-hover.
+//      a: colors must retain their original color and do not change over each re-hover. (COMPLETE)
+
 
 // 7: Make it so the colors become darker over each pass with retaining that random color.
 
@@ -32,17 +34,16 @@ container.appendChild(containerCreatedDivs)
 container.style.cssText = `grid-template-columns: repeat(${userInputGrid},1fr);`
 
 btn.addEventListener("click",() =>{
-    // container.replaceChildren is the fastest way to clear children and add news ones, in this case, we just wanted to remove.
-
-userInputGrid =  prompt("Enter your desired x by x grid");
-if(userInputGrid > 30){
-    alert("Sorry, the value must be 30 or below");
-    return;
-}
-container.replaceChildren();
+    
+    userInputGrid =  prompt("Enter your desired x by x grid");
+    if(userInputGrid > 100){
+        alert("Sorry, the value must be 100 or below");
+        return;
+    }
+    container.replaceChildren();
+    // container.replaceChildren is the fastest way to clear children and add news ones, in this case, we just wanted to remove. Speed of this and innerHTML make no difference, leave as is.
 calcsNumOfDivsToMake(userInputGrid);
 createAndAppendsDivs();
-hoverGrid = document.querySelectorAll("div");
 
 })
 
@@ -57,26 +58,29 @@ function calcsNumOfDivsToMake(userInputGrid){
 function createAndAppendsDivs(){
     for(let i = 0; i < numOfDivsToCreate; i++){
         containerCreatedDivs = document.createElement("div")
+
         container.appendChild(containerCreatedDivs)
         getDivs = document.querySelectorAll("div");
         
 
-        getDivs.forEach(e =>{
-            let att = e.getAttribute('style')
-    
-
-                e.addEventListener("mouseover", ()=>{
-                    randomColor1 = Math.floor(Math.random()*365)
-                    randomColor2 = Math.floor(Math.random()*365)
-                    randomColor3 = Math.floor(Math.random()*365)
-                    if(e.style.backgroundColor){
-                    return;
-                    }
-                    e.style.cssText =  `background-color: rgb(${randomColor1},${randomColor2},${randomColor3})`
-                })
-            
-        })
+        
         }
+        // fixed issue with lag, this callback was placed within for the for loop, causing it to be ran more times than necessary, solution, move it outside the for loop.
+        getDivs.forEach(e =>
+    
+            e.addEventListener("mouseover", ()=>{
+            
+                // if(e.style.backgroundColor){
+                // return;
+                // }
+                randomColor1 = Math.floor(Math.random()*365)
+                 randomColor2 = Math.floor(Math.random()*365)
+                randomColor3 = Math.floor(Math.random()*365)
+                e.style.cssText =  `background-color: rgb(${randomColor1} ${randomColor2} ${randomColor3})`
+                
+            })
+        
+    )
 }
 
 let getDivs = document.querySelectorAll("div");
